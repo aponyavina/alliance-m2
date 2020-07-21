555c9299695fceaa4e46e79324a917f8b10b15e3
Anna Ponyavina <ponyavina1997@mail.ru>
2020-07-21 17:38:29 +0300

    complete(main page)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 window.addEventListener('DOMContentLoaded', () => {
-'use strict';
+    'use strict';
+
+    //Scroll
+    const scroll = () => {
+        const nav = document.querySelector('.nav'),
+            buttonOrder = document.querySelector('.button-order'),
+            buttonCalc = document.querySelector('.button-calc'),
+            calc = document.getElementById('calc'),
+            request = document.getElementById('request'),
+            scrollToBlock = (block) => {
+                block.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            };
+
+        nav.addEventListener('click', (e) => {
+            e.preventDefault();
+            let target = e.target;
+            if (target.classList.contains('nav-item')) {
+                const idName = target.getAttribute('href').slice(1);
+                const idBlock = document.getElementById(idName);
+                scrollToBlock(idBlock);
+            }
+        });
+        buttonOrder.addEventListener('click', (e) => {
+            e.preventDefault();
+            scrollToBlock(request);
+        });
+        buttonCalc.addEventListener('click', (e) => {
+            e.preventDefault();
+            scrollToBlock(calc);
+        });
+    };
+    scroll();
 
     //Slider
     const slider = () => {
